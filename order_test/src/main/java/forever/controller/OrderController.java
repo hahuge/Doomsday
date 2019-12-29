b942e2f37aa98283e6b9063150663b531ba598c0
hahuge <1240697048@qq.com>
2019-12-30 00:09:04 +0800

    模拟测试3

--- order_test/src/main/java/forever/controller/OrderController.java
+++ order_test/src/main/java/forever/controller/OrderController.java
@@ -3,33 +3,32 @@ package forever.controller;
 import forever.entity.Order;
 import forever.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
     @Autowired
-    private OrderService orderService;
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/buy/{id}", method = RequestMethod.GET)
-    public Object findById(){
+    @RequestMapping(value = "/mode/test", method = RequestMethod.GET)
+    public Object getModeTest(){
         return restTemplate.getForObject("http://127.0.0.1:9001/product/1", Object.class);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Order findById(@PathVariable Long id){
-        Order order = orderService.findById(id);
-        return order;
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
-    public String findById(@RequestBody Order order){
-        orderService.save(order);
-        return "success";
+    @RequestMapping(value = "/mode/eureka", method = RequestMethod.GET)
+    public Object getModeEureka(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("product_service");
+        ServiceInstance instance = instances.get(0);
+        return restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/product/1", Object.class);
     }
 }
